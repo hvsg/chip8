a2b448f16fe21d4eda8403476faf52e6dc72afc3
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2021-05-23 14:13:36 -0700

    Added register addresses and memory space.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
+/// Big Endian
+/// Each address refers to a single byte
+/// last byte is at index 4095
+const MAX_ADDR: usize = 0xFFF;
+/// Size of the memory space in bytes
+const MEMORY_SIZE: usize = MAX_ADDR + 1;
+/// Program Counter, 16 bits:
+/// upper 8 bits at 0x0, lower 8 bits at 0x01
+const PC_ADDR: usize = 0x0;
+/// Stack Pointer
+const SP_ADDR: usize = 0x2;
+/// Base address of the stack
+const STACK_ADDR: usize = Register::ST as usize;
+/// Number of bytes used for the stack
+const STACK_SIZE: usize = 16 * 2;
+/// Stack address should be less than this address
+const STACK_END_ADDR: usize = STACK_ADDR + STACK_SIZE;
 
-const MAX_ADDR:u16 = 0xFFF;
-const MEMORY_SIZE: usize = MAX_ADDR as usize + 1;
+/// Contains 8-bit register addresses
+#[repr(usize)]
+enum Register {
+    V0 = SP_ADDR + 1,
+    V1,
+    V2,
+    V3,
+    V4,
+    V5,
+    V6,
+    V7,
+    V8,
+    V9,
+    VA,
+    VB,
+    VC,
+    VD,
+    VE,
+    VF,
+    DT,
+    ST,
+}
+
+/// Data structure for the Chip-8 Interpreter
 struct Chip8 {
-    // memory
-    memory: [u8; MEMORY_SIZE]
+    // RAM
+    memory: [u8; MEMORY_SIZE],
 }
 
 impl Chip8 {
     fn new() -> Self {
-        Self{
+        Self {
             memory: [0; MEMORY_SIZE],
         }
     }
