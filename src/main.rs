169e2c5e1c17a261ad51160dca7fb00fe1abcb86
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2021-05-23 12:59:30 -0700

    Initialize memory space.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+const MAX_ADDR:u16 = 0xFFF;
+const MEMORY_SIZE: usize = MAX_ADDR as usize + 1;
+struct Chip8 {
+    // memory
+    memory: [u8; MEMORY_SIZE]
+}
+
+impl Chip8 {
+    fn new() -> Self {
+        Self{
+            memory: [0; MEMORY_SIZE],
+        }
+    }
+}
+
 fn main() {
+    let mut chip8 = Chip8::new();
     println!("Hello, world!");
 }
