b054522fdbb8cbcaa52449e035b32c8b1e56b90c
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2021-05-25 23:59:18 -0700

    Implemented reg to BCD representation instruction.

--- src/lib.rs
+++ src/lib.rs
@@ -376,7 +376,17 @@ impl Chip8 {
         } else if i & 0xF0FF == 0xF029 {
             unimplemented!()
         } else if i & 0xF0FF == 0xF033 {
-            unimplemented!()
+            // Store BCD representation of Vx starting at I
+            let vx = to_general_reg8(((i & 0x0F00) >> 8) as usize);
+            let mut value = self.read_reg8(vx);
+            let start = self.read_reg16(Reg16::I);
+            let h = value / 100;
+            self.write8(start as usize, h);
+            value -= h * 100;
+            let t = value / 10;
+            self.write8((start + 1) as usize, t);
+            self.write8((start + 2) as usize, value - t * 10);
+            true
         } else if i & 0xF0FF == 0xF055 {
             // write v0-vx into memory starting at I
             let start = self.read_reg16(Reg16::I);
